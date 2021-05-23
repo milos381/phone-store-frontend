2aec6c8469927fd9735a0d0211bd50ee784679e6
milos381 <milos_popovic@outlook.com>
2021-05-23 19:03:06 -0400

    update

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getCurrentUser } from "../actions/userActions.js"
+import { logout } from "../actions/userActions.js"
 class Admin extends React.Component {
+
+componentDidMount() {
+    this.props.getCurrentUser()
+}
+render(){
+  
+    return (
+      <div >
+        { this.props.loggedIn ?         
+        <Switch>
+          <Route exact path='/logout' component={Logout}/>
+        </Switch> 
+        
+        :
+        
+        <Switch>
+        <Route exact path='/signup' component={SignUp}/>
+        <Route exact path='/login' component={Login}/>
+      </Switch> }
+
+      </div>
+    );
+
+  }
 
 }
 const mapStateToProps = state => {
@@ -9,5 +34,11 @@ const mapStateToProps = state => {
       loggedIn: !!state.currentUser
     })
 }
+const dispatchToProps = (dispatch) => {
+    return {
+      getCurrentUser: () => dispatch(getCurrentUser()),
+      logout: () => dispatch(logout())
+    }
+  }
 
-export default connect(mapStateToProps, { getCurrentUser })(Admin);
+export default connect(mapStateToProps, dispatchToProps)(Admin);
